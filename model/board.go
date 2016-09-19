687c4f127b4773ad4cf1e96e45e750b19926f582
kei-tamiya <webcreatorhouse@gmail.com>
2016-09-19 19:15:03 +0900

    [add] add copied file of last todo code

---
+++ model/board.go
@@ -0,0 +1,60 @@
+package model
+
+import (
+	"time"
+
+	"database/sql"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/pkg/errors"
+)
+
+// 行動予定タグとなるもの
+type Board struct {
+	ID      int64      `db:"board_id" json:"id"`
+	Date    string     `json:"date"`
+	Created *time.Time `json:created`
+	Updated *time.Time `json:updated`
+}
+
+func (t *Board) BoardsAll(dbx *sqlx.DB) (boards []Board, err error) {
+	if err = dbx.Select(&boards, "SELECT * FROM boards"); err != nil {
+		return nil, err
+	}
+	return boards, nil
+}
+
+func (t *Board) CheckLatestDate(dbx *sqlx.DB) (board Board, err error) {
+	boards := []Board{}
+	if err = dbx.Select(&boards, "select * from boards order by created desc limit 1"); err != nil {
+		return board, err
+	}
+	if len(boards) == 0 {
+		err = errors.New("Data is not exist in boards table")
+		return board, err
+	}
+	return boards[0], nil
+}
+
+func (t *Board) Insert(tx *sqlx.Tx) (sql.Result, error) {
+	stmt, err := tx.Prepare(`
+	INSERT INTO boards (date)
+	VALUES (?)
+	`)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	return stmt.Exec(t.Date)
+}
+
+func (t *Board) Update(tx *sqlx.Tx) (sql.Result, error) {
+	stmt, err := tx.Prepare(`
+	update boards set date = ? where board_id = ?
+	`)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	return stmt.Exec(t.Date, t.ID)
+}
---
+++ model/task.go
@@ -0,0 +1,62 @@
+package model
+
+import (
+	"time"
+
+	"database/sql"
+
+	"github.com/jmoiron/sqlx"
+)
+
+// 行動予定タグとなるもの
+type Todo struct {
+	ID        int64      `db:"todo_id" json:"id"`
+	BoardID   int64      `json:"boardID"`
+	Title     string     `json:"Title"`
+	Completed bool       `json:"completed`
+	Top       int64      `json:"top"`
+	Left      int64      `json:"left"`
+	Width     int64      `json:"width"`
+	Height    int64      `json:"height"`
+	Created   *time.Time `json:created`
+	Updated   *time.Time `json:updated`
+}
+
+func TodosAll(dbx *sqlx.DB) (todos []Todo, err error) {
+	if err := dbx.Select(&todos, "SELECT * FROM todos"); err != nil {
+		return nil, err
+	}
+	return todos, nil
+}
+
+func (t *Todo) Insert(tx *sqlx.Tx) (sql.Result, error) {
+	stmt, err := tx.Prepare(`
+	INSERT INTO todos (board_id, title, completed, top, left, width, height)
+	VALUES (?, ?, ?, ?, ?, ?, ?)
+	`)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	return stmt.Exec(t.BoardID, t.Title, t.Completed, t.Top, t.Left, t.Width, t.Height)
+}
+
+//func (t *Todo) fetchBoardID(tx *sqlx.Tx, date string) (sql.Result, error) {
+//	stmt, err := tx.Prepare(`
+//	SELECT board_id FROM boards WHERE date = ?
+//	`)
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//}
+func (t *Todo) Update(tx *sqlx.Tx) (sql.Result, error) {
+	stmt, err := tx.Prepare(`
+	update todos set content = ? where todo_id = ?
+	`)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	return stmt.Exec(t.Title, t.ID)
+}
