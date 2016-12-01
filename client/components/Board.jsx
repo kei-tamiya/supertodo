75cb43f11a8228cf63322049bab5a418c9ae3b05
kei-tamiya <webcreatorhouse@gmail.com>
2016-12-01 22:49:20 +0900

    [fix] fix Board.jsx according to eslint

--- client/components/Board.jsx
+++ client/components/Board.jsx
@@ -5,115 +5,89 @@ import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowCol
 const Board = () => (
   <div className="wrapContents">
     <Table
-      multiSelectable={true}
-      enableSelectAll={true}
+      multiSelectable
+      enableSelectAll
     >
       <TableHeader>
         <TableRow>
           <TableHeaderColumn>Time</TableHeaderColumn>
-          <TableHeaderColumn>Todos</TableHeaderColumn>
         </TableRow>
       </TableHeader>
       <TableBody>
         <TableRow>
           <TableRowColumn>0:00</TableRowColumn>
-          <TableRowColumn></TableRowColumn>
         </TableRow>
         <TableRow>
           <TableRowColumn>1:00</TableRowColumn>
-          <TableRowColumn></TableRowColumn>
         </TableRow>
         <TableRow>
           <TableRowColumn>2:00</TableRowColumn>
-          <TableRowColumn></TableRowColumn>
         </TableRow>
         <TableRow>
           <TableRowColumn>3:00</TableRowColumn>
-          <TableRowColumn></TableRowColumn>
         </TableRow>
         <TableRow>
           <TableRowColumn>4:00</TableRowColumn>
-          <TableRowColumn></TableRowColumn>
         </TableRow>
         <TableRow>
           <TableRowColumn>5:00</TableRowColumn>
-          <TableRowColumn></TableRowColumn>
         </TableRow>
         <TableRow>
           <TableRowColumn>6:00</TableRowColumn>
-          <TableRowColumn></TableRowColumn>
         </TableRow>
         <TableRow>
           <TableRowColumn>7:00</TableRowColumn>
-          <TableRowColumn></TableRowColumn>
         </TableRow>
         <TableRow>
           <TableRowColumn>8:00</TableRowColumn>
-          <TableRowColumn></TableRowColumn>
         </TableRow>
         <TableRow>
           <TableRowColumn>9:00</TableRowColumn>
-          <TableRowColumn></TableRowColumn>
         </TableRow>
         <TableRow>
           <TableRowColumn>10:00</TableRowColumn>
-          <TableRowColumn></TableRowColumn>
         </TableRow>
         <TableRow>
           <TableRowColumn>11:00</TableRowColumn>
-          <TableRowColumn></TableRowColumn>
         </TableRow>
         <TableRow>
           <TableRowColumn>12:00</TableRowColumn>
-          <TableRowColumn></TableRowColumn>
         </TableRow>
         <TableRow>
           <TableRowColumn>13:00</TableRowColumn>
-          <TableRowColumn></TableRowColumn>
         </TableRow>
         <TableRow>
           <TableRowColumn>14:00</TableRowColumn>
-          <TableRowColumn></TableRowColumn>
         </TableRow>
         <TableRow>
           <TableRowColumn>15:00</TableRowColumn>
-          <TableRowColumn></TableRowColumn>
         </TableRow>
         <TableRow>
           <TableRowColumn>16:00</TableRowColumn>
-          <TableRowColumn></TableRowColumn>
         </TableRow>
         <TableRow>
           <TableRowColumn>17:00</TableRowColumn>
-          <TableRowColumn></TableRowColumn>
         </TableRow>
         <TableRow>
           <TableRowColumn>18:00</TableRowColumn>
-          <TableRowColumn></TableRowColumn>
         </TableRow>
-        <TableRow selected={true}>
+        <TableRow selected>
           <TableRowColumn>19:00</TableRowColumn>
-          <TableRowColumn></TableRowColumn>
         </TableRow>
         <TableRow>
           <TableRowColumn>20:00</TableRowColumn>
-          <TableRowColumn></TableRowColumn>
         </TableRow>
         <TableRow>
           <TableRowColumn>21:00</TableRowColumn>
-          <TableRowColumn></TableRowColumn>
         </TableRow>
         <TableRow>
           <TableRowColumn>22:00</TableRowColumn>
-          <TableRowColumn></TableRowColumn>
         </TableRow>
         <TableRow>
           <TableRowColumn>23:00</TableRowColumn>
-          <TableRowColumn></TableRowColumn>
         </TableRow>
         <TableRow>
           <TableRowColumn>24:00</TableRowColumn>
-          <TableRowColumn></TableRowColumn>
         </TableRow>
       </TableBody>
     </Table>
@@ -123,8 +97,7 @@ const Board = () => (
 Board.proptypes = {
 };
 
-const mapStateToProps = () => {
-  return {};
-};
+const mapStateToProps = () => ({
+});
 
 export default connect(mapStateToProps)(Board);
