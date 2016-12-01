f21ac2920e57133fc9eb8c9e2df10e4ccb636d5c
kei-tamiya <webcreatorhouse@gmail.com>
2016-12-01 16:46:25 +0900

    [fix] fix TodoTitleInput.jsx according to eslint check

--- client/components/TodoTitleInput.jsx
+++ client/components/TodoTitleInput.jsx
@@ -1,21 +1,36 @@
 import React, { PropTypes } from 'react';
-import { GREEN, BLUE, ORANGE, DEEPGREEN } from '../constant/Color.jsx';
 import Divider from 'material-ui/Divider';
 import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
+import { ORANGE } from '../constant/Color.jsx';
 
-const TodoTitleInput = ({id, title, changeTodoTitle, updateTodo}) => (
-  <form onSubmit={(e) => {
-    e.preventDefault();
-    changeTodoTitle(e, id)}} >
+const TodoTitleInput = ({
+  id,
+  title,
+  changeTodoTitle,
+  updateTodo,
+}) => (
+  <form
+    onSubmit={(e) => {
+      e.preventDefault();
+      changeTodoTitle(e, id);
+    }}
+  >
     <Paper zDepth={2}>
-      <TextField hintText='New To Do' className={'todoText'} underlineStyle={{borderColor: ORANGE}} value={title} onChange={(e) => changeTodoTitle(e, id)} onBlur={() => updateTodo(id)} />
-    <Divider />
+      <TextField
+        hintText="New To Do"
+        className="todoText"
+        underlineStyle={{ borderColor: ORANGE }}
+        value={title}
+        onChange={e => changeTodoTitle(e, id)}
+        onBlur={() => updateTodo(id)}
+      />
+      <Divider />
     </Paper>
   </form>
 );
 
-TodoTitleInput.PropTypes = {
+TodoTitleInput.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   changeTodoTitle: PropTypes.func.isRequired,
